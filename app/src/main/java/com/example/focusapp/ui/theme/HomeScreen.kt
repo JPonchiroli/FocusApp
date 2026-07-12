d1408944f7e701b1db71e97e0ad22a942f1034b3
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-07-12 15:37:46 -0300

    feat: add styles for monitored apps card and button

--- app/src/main/java/com/example/focusapp/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/focusapp/ui/theme/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.focusapp.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -12,8 +15,14 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.*
 
 @Composable
 fun HomeScreen() {
@@ -56,6 +65,8 @@ fun Header() {
 @Composable
 fun MonitoredAppsCard() {
 
+    var isChecked by remember { mutableStateOf(false) }
+
     Card {
 
         Column(
@@ -69,12 +80,22 @@ fun MonitoredAppsCard() {
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            Checkbox(
-                checked = true,
-                onCheckedChange = {}
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
 
-            Text("TikTok")
+                Checkbox(
+                    checked = isChecked,
+                    onCheckedChange = { isChecked = it },
+                    enabled = true
+                )
+
+                Text("TikTok")
+
+            }
 
         }
 
@@ -109,14 +130,19 @@ fun StatusCard() {
 @Composable
 fun StartMonitoringButton() {
 
-    Button(
-        onClick = {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ){
+        Button(
+            onClick = {
+
+            }
+        ) {
+
+            Text("Start monitoring")
 
         }
-    ) {
-
-        Text("Start monitoring")
-
     }
 
 }
